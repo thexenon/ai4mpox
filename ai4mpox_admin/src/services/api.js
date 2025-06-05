420ba38e36c28911f322a786171ba7407b2ccda3
Derek Donkor <67352482+thexenon@users.noreply.github.com>
2025-06-05 01:56:48 +0000

    Add Cookie

--- ai4mpox_admin/src/services/api.js
+++ ai4mpox_admin/src/services/api.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 const API_BASE_URL = 'https://ai4mpox.onrender.com/api/v1';
+const cookies =
+  document.cookie.split('; ').find((row) => row.startsWith('jwt=')) || '';
 
 export const fetchItems = (params, cookie) => {
   return axios.request({
     method: 'GET',
     url: `${API_BASE_URL}/${params}/`,
-    headers: cookie ? { Cookie: cookie } : {},
+    headers: cookie ? { Cookie: cookie } : { Cookie: cookies },
     withCredentials: true,
   });
 };
@@ -14,7 +16,7 @@ export const fetchItem = (params, cookie) => {
   return axios.request({
     method: 'GET',
     url: `${API_BASE_URL}/${params}/${item.id || item._id}`,
-    headers: cookie ? { Cookie: cookie } : {},
+    headers: cookie ? { Cookie: cookie } : { Cookie: cookies },
     withCredentials: true,
   });
 };
@@ -23,7 +25,7 @@ export const deleteItem = (item, params, cookie) => {
   return axios.request({
     method: 'DELETE',
     url: `${API_BASE_URL}/${params}/${item.id || item._id}`,
-    headers: cookie ? { Cookie: cookie } : {},
+    headers: cookie ? { Cookie: cookie } : { Cookie: cookies },
     withCredentials: true,
   });
 };
